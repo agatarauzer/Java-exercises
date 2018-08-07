bbd9053c6ae17a1ccb9c4038f77ca09a6002bd39
agata <agatarauzer@gmail.com>
2018-08-07 10:06:04 +0200

    2 exercises

---
+++ Hackerrank/src/main/java/Java/Introduction/Easy/DateAndTime.java
@@ -0,0 +1,23 @@
+package Java.Introduction.Easy;
+
+import java.util.Calendar;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class DateAndTime {
+
+    public static String getDay(String day, String month, String year) {
+        Calendar cal = Calendar.getInstance();
+        cal.set(Integer.parseInt(year), Integer.parseInt(month) - 1, Integer.parseInt(day));
+        return cal.getDisplayName(Calendar.DAY_OF_WEEK, Calendar.LONG, Locale.US).toUpperCase();
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String month = in.next();
+        String day = in.next();
+        String year = in.next();
+
+        System.out.println(getDay(day, month, year));
+    }
+}

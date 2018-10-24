16c60e8cc2b32ebbe94d0726642f67fa047b8a7e
agatarauzer <agatarauzer@gmail.com>
2018-10-24 15:52:37 +0200

    days 16-18

---
+++ Hackerrank/src/main/java/ThirtyDaysOfCode/Day16/Exceptions.java
@@ -0,0 +1,17 @@
+package ThirtyDaysOfCode.Day16;
+
+import java.util.Scanner;
+
+public class Exceptions {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String S = in.next();
+
+        try {
+            System.out.println(Integer.parseInt(S));
+        }
+        catch(NumberFormatException e) {
+            System.out.println("Bad String");
+        }
+    }
+}

bc31652ff5884348961d0f93b9f133cd0f274d40
agatarauzer <agatarauzer@gmail.com>
2018-10-29 18:00:12 +0100

    day 22

---
+++ Hackerrank/src/main/java/ThirtyDaysOfCode/Day22/BinarySearchTrees/Node.java
@@ -0,0 +1,10 @@
+package ThirtyDaysOfCode.Day22.BinarySearchTrees;
+
+public class Node {
+    Node left,right;
+    int data;
+    Node(int data){
+        this.data=data;
+        left=right=null;
+    }
+}
---
+++ Hackerrank/src/main/java/ThirtyDaysOfCode/Day22/BinarySearchTrees/Solution.java
@@ -0,0 +1,45 @@
+package ThirtyDaysOfCode.Day22.BinarySearchTrees;
+
+import java.util.Scanner;
+
+public class Solution {
+    public static int getHeight(Node root) {
+
+        if (root == null) {
+            return -1;
+        } else {
+            int lDepth = getHeight(root.left);
+            int rDepth = getHeight(root.right);
+            return 1 + Math.max(lDepth, rDepth);
+        }
+    }
+    public static Node insert(Node root,int data){
+        if(root==null){
+            return new Node(data);
+        }
+        else{
+            Node cur;
+            if(data<=root.data){
+                cur=insert(root.left,data);
+                root.left=cur;
+            }
+            else{
+                cur=insert(root.right,data);
+                root.right=cur;
+            }
+            return root;
+        }
+    }
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int T=sc.nextInt();
+        Node root=null;
+        while(T-->0){
+            int data=sc.nextInt();
+            root=insert(root,data);
+        }
+        int height=getHeight(root);
+        System.out.println(height);
+    }
+
+}

3bd50da360df8873acdfb9bf20f23f4c838b9f02
agatarauzer <agatarauzer@gmail.com>
2020-10-11 22:36:38 +0200

    exercise of the day

---
+++ Codewars/src/main/java/IsANumberPrime/Prime.java
@@ -0,0 +1,15 @@
+package IsANumberPrime;
+
+public class Prime {
+    public static boolean isPrime(int num) {
+        if (num < 2) {
+            return false;
+        }
+        for (int i = 2; i*i <= num; i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}

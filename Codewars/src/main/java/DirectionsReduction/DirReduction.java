e5a5527d0d75049112e3bef47b3c15b4ba70249d
agatarauzer <agatarauzer@gmail.com>
2021-01-12 21:43:42 +0100

    codewars exercise

---
+++ Codewars/src/main/java/DirectionsReduction/DirReduction.java
@@ -0,0 +1,52 @@
+package DirectionsReduction;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class DirReduction {
+    public static String[] dirReduc(String[] arr) {
+        List<String> list = new ArrayList<>(Arrays.asList(arr));
+        int listSizeBefore = list.size();
+        int listSizeAfter = 0;
+
+        while (listSizeBefore != listSizeAfter) {
+            listSizeBefore = list.size();
+            for (int i = 0; i < list.size() - 1; i++) {
+                if (oneIsRedundant(list.get(i), list.get(i+1))) {
+                    list.remove(i+1);
+                }
+                else if (twoAreRedundant(list.get(i), list.get(i+1))) {
+                    list.remove(i+1);
+                    list.remove(i);
+                }
+            }
+            listSizeAfter = list.size();
+        }
+        String[] result = new String[list.size()];
+        result = list.toArray(result);
+        return result;
+    }
+
+    private static boolean oneIsRedundant(String dir1, String dir2) {
+        if (dir1.equals(dir2)) {
+            return true;
+        }
+        return false;
+    }
+
+    private static boolean twoAreRedundant(String dir1, String dir2) {
+        if (dir1.equals("NORTH") && dir2.equals("SOUTH")) {
+            return true;
+        }
+        if (dir1.equals("SOUTH") && dir2.equals("NORTH")) {
+            return true;
+        }
+        if (dir1.equals("EAST") && dir2.equals("WEST")) {
+            return true;
+        }
+        if (dir1.equals("WEST") && dir2.equals("EAST")) {
+            return true;
+        }
+        return false;
+    }
+}

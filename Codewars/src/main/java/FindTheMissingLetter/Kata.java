e340ab82c835da658b560a8dd95054fbd606051c
Agata Rauzer <37142511+agatarauzer@users.noreply.github.com>
2022-10-27 19:36:23 +0200

    Find the missing letter

---
+++ Codewars/src/main/java/FindTheMissingLetter/Kata.java
@@ -0,0 +1,12 @@
+public class Kata
+{
+  public static char findMissingLetter(char[] array)
+  {
+    for (int i = 1; i < array.length; i++) {
+        if (array[i] - array[i-1] != 1) {
+          return (char)(array[i]-1);
+        }
+    } 
+   return 'a';
+  }
+}
